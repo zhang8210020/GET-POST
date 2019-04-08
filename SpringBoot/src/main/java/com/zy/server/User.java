2604132b13462f59d315a8912509a4fbaf7f2d25
zhang8210020 <644348235@qq.com>
2019-04-08 18:08:13 +0800

    2019-4-8

---
+++ SpringBoot/src/main/java/com/zy/server/User.java
@@ -0,0 +1,12 @@
+package com.zy.server;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private  String UserName;
+    private  String PassWord;
+    private String name;
+    private String age;
+    private String sex;
+}
