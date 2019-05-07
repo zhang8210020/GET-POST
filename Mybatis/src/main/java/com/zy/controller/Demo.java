b073c8a0747f1dc0d264d8e0a45cdfb4a07d8727
zhang8210020 <644348235@qq.com>
2019-05-07 12:44:22 +0800

    2019.5.7

--- Mybatis/src/main/java/com/zy/controller/Demo.java
+++ Mybatis/src/main/java/com/zy/controller/Demo.java
@@ -1,26 +1,26 @@
 package com.zy.controller;
 
+import com.zy.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
-import org.apache.catalina.User;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @Log4j2
 @RestController
 @Api(value = "v1",description = "这是我第一个版本的demo")
 @RequestMapping("v1")
 public class Demo {
 
-    //首先获取一个执行sql语句的对象
+    //首先获取一个执行sql语句的语句
+
     @Autowired
     private SqlSessionTemplate template;
     @RequestMapping(value = "/getUserList",method = RequestMethod.GET)
     @ApiOperation(value = "可以获取到用户数",httpMethod = "GET")
+    //调用sql语句
     public int getUserList(){
         return template.selectOne("getUserCount");
     }
@@ -29,6 +29,14 @@ public class Demo {
     public int addUser(@RequestBody User user){
         int result = template.insert("addUser",user);
         return result;
+    }
+    @RequestMapping(value = "/updataUser",method = RequestMethod.POST)
+    public int updateUser(@RequestBody User user){
+        return template.update("updateUser",user);
+    }
+    @RequestMapping(value = "/deleteUser",method = RequestMethod.GET)
+    public int delUser(@RequestParam int id){
+       return template.delete("deleteUser",id);
     }
 
 
